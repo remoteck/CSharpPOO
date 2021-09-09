7d13a20678d3bdba629874f747c24f45e7a071cf
Guilherme Gaspar Perroni <guilhermegasparperroni@hotmail.com>
2021-09-09 14:15:35 -0300

    Implementado método sacar e depositar

--- ByteBank/ContaCorrente.cs
+++ ByteBank/ContaCorrente.cs
@@ -1,7 +1,26 @@
-﻿
+﻿using System;
+
 public class ContaCorrente {
     public string titular;
     public int agenciaNum;
     public int contaNum;
     public double saldo;
+
+    public bool Sacar(double valor) {
+        if (this.saldo < valor) {
+            Console.WriteLine($"Saldo inválido!\nSaldo Atual: {this.saldo}");
+            return false;
+        }
+        else {
+            this.saldo -= valor;
+            Console.WriteLine($"Saque Realizado!\nSaldo Atual: {this.saldo}");
+            return true;
+        }
+    }
+
+    public void Depositar(double valor) {
+        this.saldo += valor;
+        Console.WriteLine($"Deposito Realizado!\nSaldo Atual: {this.saldo}");
+    }
+
 }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,24 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ByteBank {
     class Program {
         static void Main(string[] args) {
 
             ContaCorrente contaLeona = new ContaCorrente() {
-                titular = "Leona Figueiredo Perroni",
-                agenciaNum = 260821,
+                titular = "Leona",
+                agenciaNum = 1,
                 contaNum = 1,
-                saldo = 1000
+                saldo = 1500
             };
 
-            Console.WriteLine($"Titular da Conta: {contaLeona.titular}");
-            Console.WriteLine($"Agência: {contaLeona.agenciaNum}");
-            Console.WriteLine($"Conta: {contaLeona.contaNum}");
-            Console.WriteLine($"Saldo: R${contaLeona.saldo}");
+            contaLeona.Sacar(78);
+            contaLeona.Depositar(500);
 
             Console.ReadLine();
         }
